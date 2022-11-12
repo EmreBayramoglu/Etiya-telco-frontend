870ad98e484285ed8bbfc9aebbdf4b1f0461894a
Emre BAYRAMOĞLU <emrebayramoglu@yaani.com>
2022-11-12 22:21:48 +0300

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AssignCatalogComponent } from './pages/assign-catalog/assign-catalog.component';
+import { CreateCustomerComponent } from './pages/create-customer/create-customer.component';
+import { CustomerComponent } from './pages/customer/customer.component';
+import { CustomerDetailComponent } from './pages/customer-detail/customer-detail.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { ListViewComponent } from './components/list-view/list-view.component';
+import { LoginComponent } from './pages/login/login.component';
+import { LoginGuard } from './guards/login.guard';
+import { NgModule } from '@angular/core';
+import { SummaryComponent } from './pages/summary/summary.component';
+
+const routes: Routes = [
+  {path:'', redirectTo:'login', pathMatch:'full'},
+  {path:'services', component: HomepageComponent, canActivate:[LoginGuard]},
+  {path:'login', component: LoginComponent},
+  {path:'customers', component: CustomerComponent, canActivate:[LoginGuard]},
+  {path: 'customerDetail/:id', component: CustomerDetailComponent, canActivate:[LoginGuard]},
+  {path: 'createCustomer', component: CreateCustomerComponent, canActivate:[LoginGuard]},
+  {path: 'catalog', component: AssignCatalogComponent, canActivate:[LoginGuard]},
+  {path: 'summary', component: SummaryComponent, canActivate:[LoginGuard]}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,77 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppStoreState } from './store/app.state';
+import { AssignCatalogComponent } from './pages/assign-catalog/assign-catalog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { CreateCustomerComponent } from './pages/create-customer/create-customer.component';
+import { CreateFakeArrayPipe } from './pipes/create-fake-array.pipe';
+import { CustomerComponent } from './pages/customer/customer.component';
+import { CustomerDetailComponent } from './pages/customer-detail/customer-detail.component';
+// import { CustomerToregisterReducer } from './store/customerToRegister'
+import { FilterPipe } from './pipes/filter.pipe';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { JwtModule } from "@auth0/angular-jwt";
+import { ListViewComponent } from './components/list-view/list-view.component';
+import { LoadingInterceptor } from './interceptors/loading.interceptor';
+import { LoadingOverlayComponent } from './components/loading-overlay/loading-overlay.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NgModule } from '@angular/core';
+import { SplitPipe } from './pipes/split.pipe';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from '@ngrx/store';
+import { SummaryComponent } from './pages/summary/summary.component';
+import { ToastrModule } from 'ngx-toastr';
+import { TokenInterceptor } from './interceptors/token.interceptor';
+import { appReducers } from './store/app.reducer';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListViewComponent,
+    CreateFakeArrayPipe,
+    SplitPipe,
+    LoginComponent,
+    HomepageComponent,
+    LoadingOverlayComponent,
+    NavbarComponent,
+    FooterComponent,
+    CustomerComponent,
+    FilterPipe,
+    CustomerDetailComponent,
+    CreateCustomerComponent,
+    AssignCatalogComponent,
+    SummaryComponent,
   ],
+  
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    AppRoutingModule,
+    HttpClientModule,//ekledik
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: () => {return localStorage.getItem('token');}
+      },
+    }),
+    StoreModule.forRoot<AppStoreState>(appReducers),
+    StoreDevtoolsModule.instrument({
+      autoPause: false,
+    }),
   ],
-  providers: [],
+
+  // Injectable classlarımı providers 'ta tanımlarım
+  providers: [{provide: HTTP_INTERCEPTORS,useClass:LoadingInterceptor,multi:true},{provide: HTTP_INTERCEPTORS,useClass:TokenInterceptor,multi:true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
